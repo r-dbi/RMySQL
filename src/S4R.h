b6f714e14fb047f6c18886cb76a837866476efff
dj <dj@dcde13d4-9b1b-0410-ac9e-ef07de68c835>
2002-05-17 22:48:24 +0000

    init from 0.4-3
    git-svn-id: https://hedgehog.fhcrc.org/compbio/r-dbi@31 dcde13d4-9b1b-0410-ac9e-ef07de68c835

--- src/S4R.h
+++ src/S4R.h
@@ -1,4 +1,5 @@
-/* S4 (Splus5+) and R portability macros.
+/* $Id$
+ * S4 (Splus5+) and R portability macros.
  *
  * This file provides additional macros to the ones in Rdefines.h (in R)
  * and S4/Splus5 S.h (see Appendix A of the green book) to 
