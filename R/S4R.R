5ab138e36748e16ecaadf7cf08f2db6924444818
dj <dj@dcde13d4-9b1b-0410-ac9e-ef07de68c835>
2002-05-18 18:01:10 +0000

    minor things
    git-svn-id: https://hedgehog.fhcrc.org/compbio/r-dbi@39 dcde13d4-9b1b-0410-ac9e-ef07de68c835

--- R/S4R.R
+++ R/S4R.R
@@ -1,93 +1,43 @@
+## 
+## $Id$
+##
 ## This file defines some functions that mimic S4 functionality,
-## for R versions prior to 1.4.
-## Functions: oldClass, "oldClass<-", as, new, and show
+## namely:  new, as, show.
 
-"usingR" <- 
-function(major=0, minor=0)
-## are we using at least R version major, minor?
-{
-   if(is.null(version$language))
-      return(FALSE)
-   if(version$language!="R")
-      return(FALSE)
-   version$major>=major && version$minor>=minor
-}
-
-"usingSplus" <- 
-function(major, minor)
-## are we using at least Splus version major, minor?
-{
-   if(!is.null(version$language) && version$language=="R")
-      return(FALSE)
-   version$major>=major && version$minor>=minor
-}
-
-"usingV4" <- 
-function()
-## are we using V4-style classes and methods? (this 
-## applies to either R or Splus)
-{
-   exists("setClass", mode = "function")
+usingR <- function(major=0, minor=0){
+  if(is.null(version$language))
+    return(FALSE)
+  if(version$language!="R")
+    return(FALSE)
+  version$major>=major && version$minor>=minor
 }
 
 ## constant holding the appropriate error class returned by try() 
 if(usingR()){
   ErrorClass <- "try-error"
 } else {
-  ErrorClass <- "Error"      ## Splus
-}
-
-if(!exists("existsFunction", mode = "function")){
-    existsFunction <- function(f) exists(f, mode = "function")
-}
-
-if(!existsFunction("oldClass")){
-   "oldClass" <- class
-   "oldClass<-" <- 
-   function(x, value) 
-   {
-      class(x) <- value
-      x
-   }
+  ErrorClass <- "Error"  
 }
 
 if(TRUE){
-
-   "as" <- 
-   function(object, classname)
+   ## When we move to version 4 style classes, we should replace 
+   ## the calls to the following by their lower-case counerpart, 
+   ## as defined in library(methods)
+   AS <- function(object, classname)
    {
-      get(paste("as", as.character(classname), sep = "."))(object)
+     get(paste("as", as.character(classname), sep = "."))(object)
    }
 
-   "new" <- 
-   function(classname, ...) 
+   NEW <- function(classname, ...)
    {
-      if(!is.character(classname))
-         stop("classname must be a character string")
-      class(classname) <- classname
-      UseMethod("new")
+     if(!is.character(classname))
+       stop("classname must be a character string")
+     do.call(paste("NEW", classname[1], sep="."), list(...))
    }
 
-   "new.default" <- 
-   function(classname, ...) 
+   NEW.default <- function(classname, ...)
    {
-      #structure(list(...), class = unclass(classname))
-      x <- list()
-      attributes(x) <- list(...)
-      class(x) <- unclass(classname)
-      x
+     structure(list(...), class = unclass(classname))
    }
 
-   "show" <- 
-   function(object, ...) 
-   {
-      UseMethod("show")
-   }
-
-   "show.default" <- 
-   function(object) 
-   {
-      print(object)
-      invisible(NULL)
-   }
 }
