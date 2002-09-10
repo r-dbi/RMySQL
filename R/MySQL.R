c94e43577291f4869513802b892ae80d07644a40
dj <dj@dcde13d4-9b1b-0410-ac9e-ef07de68c835>
2002-09-10 11:50:46 +0000

    changed license from LGPL to GPL
    git-svn-id: https://hedgehog.fhcrc.org/compbio/r-dbi@80 dcde13d4-9b1b-0410-ac9e-ef07de68c835

--- R/MySQL.R
+++ R/MySQL.R
@@ -4,14 +4,14 @@
 ## Copyright (C) 1999 The Omega Project for Statistical Computing.
 ##
 ## This library is free software; you can redistribute it and/or
-## modify it under the terms of the GNU General Public
-## License as published by the Free Software Foundation; either
+## modify it under the terms of the GNU General Public License
+## as published by the Free Software Foundation; either
 ## version 2 of the License, or (at your option) any later version.
 ##
 ## This library is distributed in the hope that it will be useful,
 ## but WITHOUT ANY WARRANTY; without even the implied warranty of
 ## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-## Lesser General Public License for more details.
+## General Public License for more details.
 ##
 ## You should have received a copy of the GNU General Public
 ## License along with this library; if not, write to the Free Software
