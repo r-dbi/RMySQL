301d55dd499cdfd5ae14183c399a2766a1792cea
dj <dj@dcde13d4-9b1b-0410-ac9e-ef07de68c835>
2003-12-02 16:01:04 +0000

    fixed wrong arg to library
    git-svn-id: https://hedgehog.fhcrc.org/compbio/r-dbi@139 dcde13d4-9b1b-0410-ac9e-ef07de68c835

--- R/zzz.R
+++ R/zzz.R
@@ -5,7 +5,7 @@
 ".conflicts.OK" <- TRUE
 ## need DBI and methods *prior* to having library.dynam() invoked!
 library(methods)
-library(DBI, quietly = TRUE, warn.conflicts = FALSE)
+library(DBI, warn.conflicts = FALSE)
 
 ".First.lib" <- 
 function(lib, pkg) 
