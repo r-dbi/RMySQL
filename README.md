4f86d01364fb10e3a6b28f8a2b2fe0791bb1996a
Jeroen Ooms <jeroenooms@gmail.com>
2014-12-20 19:16:21 -0800

    Moved repository to rstats-db

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 RMySQL
 ======
 
-[![Build Status](https://travis-ci.org/jeroenooms/RMySQL.svg?branch=master)](https://travis-ci.org/jeroenooms/RMySQL)
+[![Build Status](https://travis-ci.org/rstats-db/RMySQL.svg?branch=master)](https://travis-ci.org/rstats-db/RMySQL)
 
 RMySQL is a database interface and MySQL driver for R. This version complies with the database interface definition as implemented in the package DBI 0.2-2. 
--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,7 +1,7 @@
 # Build against static libraries from curl website.
 if(!file.exists("../windows/mariadb-5.5/include/mysql.h")){
   setInternet2()
-  download.file("http://jeroenooms.github.io/RMySQL/mariadb-5.5.zip", "lib.zip", quiet = TRUE)
+  download.file("http://rstats-db.github.io/RMySQL/mariadb-5.5.zip", "lib.zip", quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
   unzip("lib.zip", exdir = "../windows")
   unlink("lib.zip")
