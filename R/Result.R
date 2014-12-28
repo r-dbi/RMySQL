fed40fa71b4f878e7a849e8bcc1d24451c46805a
hadley <h.wickham@gmail.com>
2014-12-29 12:49:25 +1300

    Improve docs for result

--- R/Result.R
+++ R/Result.R
@@ -3,98 +3,50 @@
 #' MySQL's query results class.  This classes encapsulates the result of an SQL
 #' statement (either \code{select} or not).
 #'
-#'
-#' @name MySQLResult-class
-#' @docType class
-#' @section Generators: The main generator is \code{\link[DBI]{dbSendQuery}}.
-#' @seealso DBI base classes:
-#'
-#' \code{\link[DBI]{DBIObject-class}} \code{\link[DBI]{DBIDriver-class}}
-#' \code{\link[DBI]{DBIConnection-class}} \code{\link[DBI]{DBIResult-class}}
-#'
-#' MySQL classes:
-#'
-#' \code{\link{MySQLObject-class}} \code{\link{MySQLDriver-class}}
-#' \code{\link{MySQLConnection-class}} \code{\link{MySQLResult-class}}
-#' @references See the Database Interface definition document \code{DBI.pdf} in
-#' the base directory of this package or
-#' \url{http://developer.r-project.org/db}.
-#' @keywords database interface classes
-#' @examples
-#' \dontrun{
-#' drv <- dbDriver("MySQL")
-#' con <- dbConnect(drv, dbname = "rsdbi.db")
-#' }
-#'
+#' @export
+#' @keywords internal
 setClass("MySQLResult", representation("DBIResult", "MySQLObject"))
 
-setAs("MySQLResult", "MySQLConnection",
-  def = function(from) new("MySQLConnection", Id = as(from, "integer")[1:3])
-)
-setAs("MySQLResult", "MySQLDriver",
-  def = function(from) new("MySQLDriver", Id = as(from, "integer")[1:2])
-)
+setAs("MySQLResult", "MySQLDriver", function(from) {
+  new("MySQLDriver", Id = as(from, "integer")[1:2])
+})
+setAs("MySQLResult", "MySQLConnection", function(from) {
+  new("MySQLConnection", Id = as(from, "integer")[1:3])
+})
 
-setMethod("dbClearResult", "MySQLResult",
-  function(res, ...) {
-    if(!isIdCurrent(res))
-      return(TRUE)
-    rsId <- as(res, "integer")
-    .Call("RS_MySQL_closeResultSet", rsId, PACKAGE = .MySQLPkgName)
-  }
-)
-
-
-#' Fetch records from a previously executed query
+#' Execute a SQL statement on a database connection.
 #'
-#' This method is a straight-forward implementation of the corresponding
-#' generic function.
+#' To retrieve results a chunk at a time, use \code{dbSendQuery},
+#' \code{dbFetch}, then \code{dbClearResult}. Alternatively, if you want all the
+#' results (and they'll fit in memory) use \code{dbGetQuery} which sends,
+#' fetches and clears for you.
 #'
-#' The \code{RMySQL} implementations retrieves only \code{n} records, and if
-#' \code{n} is missing it only returns up to \code{fetch.default.rec} as
-#' specified in the call to \code{\link{MySQL}} (500 by default).
-#'
-#' @name fetch-methods
-#' @aliases fetch-methods fetch,MySQLResult,numeric-method
-#' fetch,MySQLResult,missing-method
-#' @docType methods
-#' @section Methods: \describe{
-#'
-#' \item{res}{ an \code{MySQLResult} object.  } \item{n}{ maximum number of
-#' records to retrieve per fetch.  Use \code{n = -1} to retrieve all pending
-#' records; use a value of \code{n = 0} for fetching the default number of rows
-#' \code{fetch.default.rec} defined in the \code{\link{MySQL}} initialization
-#' invocation.  } \item{list()}{currently not used.}\item{ }{currently not
-#' used.} }
-#' @seealso \code{\link{MySQL}}, \code{\link[DBI]{dbConnect}},
-#' \code{\link[DBI]{dbSendQuery}}, \code{\link[DBI]{dbGetQuery}},
-#' \code{\link[DBI]{dbClearResult}}, \code{\link[DBI]{dbCommit}},
-#' \code{\link[DBI]{dbGetInfo}}, \code{\link[DBI]{dbReadTable}}.
-#' @references See the Database Interface definition document \code{DBI.pdf} in
-#' the base directory of this package or
-#' \url{http://stat.bell-labs.com/RS-DBI}.
-#' @keywords methods interface database
+#' @param conn an \code{\linkS4class{SQLiteConnection}} object.
+#' @param statement a character vector of length one specifying the SQL
+#'   statement that should be executed.  Only a single SQL statment should be
+#'   provided.
+#' @param ... Unused. Needed for compatibility with generic.
+#' @export
 #' @examples
 #' \dontrun{
-#' drv <- dbDriver("MySQL")
-#' con <- dbConnect(drv, user = "opto", password="pure-light",
-#'                  host = "localhost", dbname="lasers")
-#' res <- dbSendQuery(con, statement = paste(
-#'                       "SELECT w.laser_id, w.wavelength, p.cut_off",
-#'                       "FROM WL w, PURGE P",
-#'                       "WHERE w.laser_id = p.laser_id",
-#'                       "ORDER BY w.laser_id"))
-#' # we now fetch the first 100 records from the resultSet into a data.frame
-#' data1 <- fetch(res, n = 100)
-#' dim(data1)
+#' con <- dbConnect(RMySQL::MySQL())
+#' dbWriteTable(con, "arrests", datasets::USArrests)
 #'
+#' # Run query to get results as dataframe
+#' dbGetQuery(con, "SELECT * FROM arrests limit 3")
+#'
+#' # Send query to pull requests in batches
+#' res <- dbSendQuery(con, "SELECT * FROM arrests")
+#' data <- fetch(res, n = 2)
+#' data
 #' dbHasCompleted(res)
 #'
-#' # let's get all remaining records
-#' data2 <- fetch(res, n = -1)
-#' }
+#' dbListResults(con)
+#' dbClearResult(res)
 #'
-NULL
+#' dbDisconnect(con)
+#' }
+#' @name query
 setMethod("fetch", signature(res="MySQLResult", n="numeric"),
   def = function(res, n, ...){
     n <- as(n, "integer")
@@ -111,24 +63,77 @@ setMethod("fetch", signature(res="MySQLResult", n="numeric"),
   }
 )
 
-setMethod("fetch",
-  signature(res="MySQLResult", n="missing"),
-  function(res, n, ...) fetch(res, n = 0, ...)
-)
+#' @param n maximum number of records to retrieve per fetch. Use \code{-1} to
+#'    retrieve all pending records; use \code{0} for to fetch the default
+#'    number of rows as defined in \code{\link{SQLite}}
+#' @rdname query
+#' @export
+setMethod("fetch", c("MySQLResult", "missing"), function(res, n, ...) {
+  fetch(res, n = 0, ...)
+})
 
-setMethod("dbGetInfo", "MySQLResult",
-  function(dbObj, what = "", ...) {
-    if(!isIdCurrent(dbObj))
-      stop(paste("expired", class(dbObj), deparse(substitute(dbObj))))
-    id <- as(dbObj, "integer")
-    info <- .Call("RS_MySQL_resultSetInfo", id, PACKAGE = .MySQLPkgName)
-    if(!missing(what))
-      info[what]
-    else
-      info
+#' @rdname query
+#' @export
+setMethod("dbSendQuery", c("MySQLConnection", "character"),
+  function(conn, statement) {
+    if(!isIdCurrent(conn))
+      stop(paste("expired", class(conn)))
+    conId <- as(conn, "integer")
+    statement <- as(statement, "character")
+    rsId <- .Call("RS_MySQL_exec", conId, statement, PACKAGE = .MySQLPkgName)
+    new("MySQLResult", Id = rsId)
   }
 )
 
+#' @rdname query
+#' @export
+setMethod("dbGetQuery", c("MySQLConnection", "character"),
+  function(conn, statement) {
+    if(!isIdCurrent(conn)) stop(paste("expired", class(conn)))
+
+    ## are there resultSets pending on con?
+    .clearResultSets(conn)
+
+    rs <- dbSendQuery(conn, statement)
+    if(dbHasCompleted(rs)){
+      dbClearResult(rs)            ## no records to fetch, we're done
+      invisible()
+      return(NULL)
+    }
+    res <- fetch(rs, n = -1)
+    if(dbHasCompleted(rs))
+      dbClearResult(rs)
+    else
+      warning("pending rows")
+    res
+  }
+)
+
+#' @rdname query
+#' @export
+setMethod("dbClearResult", "MySQLResult", function(res, ...) {
+  if(!isIdCurrent(res))
+    return(TRUE)
+  rsId <- as(res, "integer")
+  .Call("RS_MySQL_closeResultSet", rsId, PACKAGE = .MySQLPkgName)
+})
+
+
+#' @rdname query
+#' @export
+setMethod("dbGetInfo", "MySQLResult", function(dbObj, what = "", ...) {
+  if(!isIdCurrent(dbObj))
+    stop(paste("expired", class(dbObj), deparse(substitute(dbObj))))
+  id <- as(dbObj, "integer")
+  info <- .Call("RS_MySQL_resultSetInfo", id, PACKAGE = .MySQLPkgName)
+  if(!missing(what))
+    info[what]
+  else
+    info
+})
+
+#' @rdname query
+#' @export
 setMethod("dbGetStatement", "MySQLResult",
   def = function(res, ...){
     st <- dbGetInfo(res, "statement")[[1]]
@@ -139,6 +144,8 @@ setMethod("dbGetStatement", "MySQLResult",
   valueClass = "character"
 )
 
+#' @rdname query
+#' @export
 setMethod("dbListFields",
   signature(conn="MySQLResult", name="missing"),
   def = function(conn, name, ...){
@@ -150,62 +157,88 @@ setMethod("dbListFields",
   valueClass = "character"
 )
 
-setMethod("dbColumnInfo", "MySQLResult",
-  function(res, ...) {
-    flds <- dbGetInfo(res, "fieldDescription")[[1]][[1]]
-    if(!is.null(flds)){
-      flds$Sclass <- .Call("RS_DBI_SclassNames", flds$Sclass,
-        PACKAGE = .MySQLPkgName)
-      flds$type <- .Call("RS_MySQL_typeNames", as.integer(flds$type),
-        PACKAGE = .MySQLPkgName)
-      ## no factors
-      structure(flds, row.names = paste(seq(along=flds$type)),
-        class = "data.frame")
-    }
-    else data.frame(flds)
+#' Database interface meta-data.
+#'
+#' See documentation of generics for more details.
+#'
+#' @param res An object of class \code{\linkS4class{SQLiteResult}}
+#' @param ... Ignored. Needed for compatibility with generic
+#' @examples
+#' \dontrun{
+#' con <- dbConnect(RMySQL::MySQL())
+#' dbWriteTable(con, "t1", datasets::USArrests)
+#'
+#' rs <- dbSendQuery(con, "SELECT * FROM t1 WHERE UrbanPop >= 80")
+#' dbGetStatement(rs)
+#' dbHasCompleted(rs)
+#'
+#' info <- dbGetInfo(rs)
+#' names(info)
+#' info$fields
+#'
+#' dbClearResult(rs)
+#' dbDisconnect(con)
+#' }
+#' @name sqlite-meta
+NULL
+
+#' @export
+#' @rdname sqlite-meta
+setMethod("dbColumnInfo", "MySQLResult", function(res, ...) {
+  flds <- dbGetInfo(res, "fieldDescription")[[1]][[1]]
+  if(!is.null(flds)){
+    flds$Sclass <- .Call("RS_DBI_SclassNames", flds$Sclass,
+      PACKAGE = .MySQLPkgName)
+    flds$type <- .Call("RS_MySQL_typeNames", as.integer(flds$type),
+      PACKAGE = .MySQLPkgName)
+    ## no factors
+    structure(flds, row.names = paste(seq(along=flds$type)),
+      class = "data.frame")
   }
+  else data.frame(flds)
+}
 )
 
-setMethod("dbGetRowsAffected", "MySQLResult",
-  def = function(res, ...) dbGetInfo(res, "rowsAffected")[[1]],
-  valueClass = "numeric"
-)
+#' @export
+#' @rdname sqlite-meta
+setMethod("dbGetRowsAffected", "MySQLResult", function(res, ...) {
+  dbGetInfo(res, "rowsAffected")[[1]]
+})
 
-setMethod("dbGetRowCount", "MySQLResult",
-  def = function(res, ...) dbGetInfo(res, "rowCount")[[1]],
-  valueClass = "numeric"
-)
+#' @export
+#' @rdname sqlite-meta
+setMethod("dbGetRowCount", "MySQLResult", function(res, ...) {
+  dbGetInfo(res, "rowCount")[[1]]
+})
 
-setMethod("dbHasCompleted", "MySQLResult",
-  def = function(res, ...) dbGetInfo(res, "completed")[[1]] == 1,
-  valueClass = "logical"
-)
+#' @export
+#' @rdname sqlite-meta
+setMethod("dbHasCompleted", "MySQLResult", function(res, ...) {
+  dbGetInfo(res, "completed")[[1]] == 1
+})
 
-setMethod("dbGetException", "MySQLResult",
-  def = function(conn, ...){
-    id <- as(conn, "integer")[1:2]
-    .Call("RS_MySQL_getException", id, PACKAGE = .MySQLPkgName)
-  },
-  valueClass = "list"    ## TODO: should be a DBIException?
-)
+#' @export
+#' @rdname sqlite-meta
+setMethod("dbGetException", "MySQLResult", function(conn, ...) {
+  id <- as(conn, "integer")[1:2]
+  .Call("RS_MySQL_getException", id, PACKAGE = .MySQLPkgName)
+})
 
-setMethod("summary", "MySQLResult",
-  function(object, verbose = FALSE, ...) {
-
-    if(!isIdCurrent(object)){
-      print(object)
-      invisible(return(NULL))
-    }
+#' @export
+setMethod("summary", "MySQLResult", function(object, verbose = FALSE, ...) {
+  if(!isIdCurrent(object)){
     print(object)
-    cat("  Statement:", dbGetStatement(object), "\n")
-    cat("  Has completed?", if(dbHasCompleted(object)) "yes" else "no", "\n")
-    cat("  Affected rows:", dbGetRowsAffected(object), "\n")
-    cat("  Rows fetched:", dbGetRowCount(object), "\n")
-    flds <- dbColumnInfo(object)
-    if(verbose && !is.null(flds)){
-      cat("  Fields:\n")
-      out <- print(dbColumnInfo(object))
-    }
-    invisible(NULL)
+    invisible(return(NULL))
   }
-)
+  print(object)
+  cat("  Statement:", dbGetStatement(object), "\n")
+  cat("  Has completed?", if(dbHasCompleted(object)) "yes" else "no", "\n")
+  cat("  Affected rows:", dbGetRowsAffected(object), "\n")
+  cat("  Rows fetched:", dbGetRowCount(object), "\n")
+  flds <- dbColumnInfo(object)
+  if(verbose && !is.null(flds)){
+    cat("  Fields:\n")
+    out <- print(dbColumnInfo(object))
+  }
+  invisible(NULL)
+})
