fafd775962504164ad59cf7882f5d5aff5b21da4
hadley <h.wickham@gmail.com>
2014-12-30 09:58:05 +1300

    Implement transactions

--- R/transactions.R
+++ R/transactions.R
@@ -4,16 +4,48 @@ NULL
 #' DBMS Transaction Management
 #'
 #' Commits or roll backs the current transaction in an MySQL connection.
+#' Note that in MySQL DDL statements (e.g. \code{CREATE TABLE}) can not
+#' be rolled back.
 #'
 #' @param conn a \code{MySQLConnection} object, as produced by
 #'  \code{\link{dbConnect}}.
 #' @param ... Unused.
-setMethod("dbCommit", "MySQLConnection",
-  def = function(conn, ...) .NotYetImplemented()
-)
+#' @examples
+#' if (mysqlHasDefault()) {
+#' con <- dbConnect(RMySQL::MySQL())
+#' df <- data.frame(id = 1:5)
+#'
+#' dbWriteTable(con, "df", df)
+#' dbBegin(con)
+#' dbGetQuery(con, "UPDATE df SET id = id * 10")
+#' dbGetQuery(con, "SELECT id FROM df")
+#' dbRollback(con)
+#'
+#' dbGetQuery(con, "SELECT id FROM df")
+#'
+#' dbRemoveTable(con, "df")
+#' dbDisconnect(con)
+#' }
+#' @name transactions
+NULL
 
 #' @export
-#' @rdname dbCommit-MySQLConnection-method
-setMethod("dbRollback", "MySQLConnection",
-  def = function(conn, ...) .NotYetImplemented()
-)
+#' @rdname transactions
+setMethod("dbCommit", "MySQLConnection", function(conn, ...) {
+  dbGetQuery(con, "COMMIT")
+  TRUE
+})
+
+#' @export
+#' @rdname transactions
+setMethod("dbBegin", "MySQLConnection", function(conn, ...) {
+  dbGetQuery(con, "START TRANSACTION")
+  TRUE
+})
+
+#' @export
+#' @rdname transactions
+setMethod("dbRollback", "MySQLConnection", function(conn, ...) {
+  dbGetQuery(con, "ROLLBACK")
+  TRUE
+})
