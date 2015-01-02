4eb0e6d8625f5af2a787177b4761c7cc6fcdef34
Jeroen Ooms <jeroenooms@gmail.com>
2015-01-02 13:30:38 -0800

    Requested by CRAN

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,4 +1,6 @@
-# Build against static libraries from curl website.
+# Link against mariadb native client static libraries
+# Info and source code: https://downloads.mariadb.org/client-native/
+# Copy of source (as per license requirements): http://rstats-db.github.io/RMySQL/mariadb_client-2.0.0-src.tar.gz
 if(!file.exists("../windows/mariadb-5.5/include/mysql.h")){
   setInternet2()
   download.file("http://rstats-db.github.io/RMySQL/mariadb-5.5.zip", "lib.zip", quiet = TRUE)
