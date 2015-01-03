339ea387550103223340323cbdb10d6a6efd224b
Jeroen Ooms <jeroenooms@gmail.com>
2015-01-03 13:35:21 -0800

    Update text

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,6 +1,6 @@
 # Link against mariadb native client static libraries
 # Info and source code: https://downloads.mariadb.org/client-native/
-# Copy of source (as per license requirements): http://rstats-db.github.io/RMySQL/mariadb_client-2.0.0-src.tar.gz
+# Source copy: http://rstats-db.github.io/RMySQL/mariadb_client-2.0.0-src.tar.gz
 if(!file.exists("../windows/mariadb-client-2.0/include/mysql.h")){
   setInternet2()
   download.file("http://rstats-db.github.io/RMySQL/mariadb-client-2.0.zip", "lib.zip", quiet = TRUE)
