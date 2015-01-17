bf580b699b6b3d34950b3f5e9b486b36df1a512a
Jeroen <jeroenooms@gmail.com>
2015-01-16 17:34:12 -0800

    Update lib url

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -3,7 +3,7 @@
 # Source copy: http://rstats-db.github.io/RMySQL/mariadb_client-2.0.0-src.tar.gz
 if(!file.exists("../windows/mariadb-client-2.0/include/mysql.h")){
   setInternet2()
-  download.file("http://rstats-db.github.io/RMySQL/mariadb-client-2.0.zip", "lib.zip", quiet = TRUE)
+  download.file("http://rstats-db.github.io/RMySQL/mariadb-client-2.0-openssl.zip", "lib.zip", quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
   unzip("lib.zip", exdir = "../windows")
   unlink("lib.zip")
