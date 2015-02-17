2eaacd226f089166ced4f8873c539dadcd29b70e
hadley <h.wickham@gmail.com>
2015-02-17 06:19:08 -0600

    Use temporary tables in tests

--- tests/testthat/test-queries.R
+++ tests/testthat/test-queries.R
@@ -2,29 +2,28 @@ context("queries")
 
 test_that("query returns expected number of rows", {
   conn <- mysqlDefault()
-  dbRemoveTable(conn, "iris")
-  dbWriteTable(conn, 'iris', datasets::iris, row.names = FALSE)
+  dbWriteTable(conn, 'iris', datasets::iris, temporary = TRUE)
   rs <- dbSendQuery(conn, "SELECT * FROM iris WHERE Species='versicolor'")
 
   x <- dbFetch(rs, n = 2)
   expect_equal(nrow(x), 2)
   expect_equal(dbGetRowCount(rs), 2)
 
+  x <- dbFetch(rs, n = 2)
+  expect_equal(nrow(x), 2)
+  expect_equal(dbGetRowCount(rs), 4)
+
   dbClearResult(rs)
-  dbRemoveTable(conn, "iris")
   dbDisconnect(conn)
 })
 
 test_that("correctly computes affected rows", {
   conn <- mysqlDefault()
-  dbRemoveTable(conn, "iris")
-  dbWriteTable(conn, 'iris', datasets::iris, row.names = FALSE)
+  dbWriteTable(conn, 'iris', datasets::iris, temporary = TRUE)
   rs <- dbSendQuery(conn, "DELETE FROM iris WHERE Species = 'versicolor'")
 
   expect_equal(dbGetRowsAffected(rs), sum(iris$Species == 'versicolor'))
 
   dbClearResult(rs)
-  dbRemoveTable(conn, "iris")
   dbDisconnect(conn)
 })
-
