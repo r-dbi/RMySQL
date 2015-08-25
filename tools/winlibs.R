fbf8344f68da7287e7d208486ab8e3070b3926f9
Jeroen Ooms <jeroenooms@gmail.com>
2015-08-25 12:13:49 +0200

    Fix for R-devel on Windows

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -2,7 +2,7 @@
 # Info and source code: https://downloads.mariadb.org/client-native/
 # Source copy: http://rstats-db.github.io/RMySQL/mariadb_client-2.0.0-src.tar.gz
 if(!file.exists("../windows/libmariadbclient-2.1.0/include/mysql.h")){
-  setInternet2()
+  if(getRversion() < "3.3.0") setInternet2()
   download.file("https://github.com/rwinlib/libmariadbclient/archive/v2.1.0.zip", "lib.zip", quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
   unzip("lib.zip", exdir = "../windows")
