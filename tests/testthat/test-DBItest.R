509f1bb15caecd881ebc86b2b05b89fba1447716
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-10-10 20:35:02 +0200

    no 64-bit support

--- tests/testthat/test-DBItest.R
+++ tests/testthat/test-DBItest.R
@@ -1,2 +1,2 @@
 DBItest::make_context(MySQL(), list(dbname = "test"))
-DBItest::test_all(c("constructor_strict", "show", "get_info", "invalid_query", "data_logical"))
+DBItest::test_all(c("constructor_strict", "show", "get_info", "invalid_query", "data_logical", "data_64_bit"))
