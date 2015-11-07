dbcd888e03b8dcad828f1c0f78fda4dfd823d9fb
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-11-07 15:51:09 +0100

    skip new tests where already a subset was failing

--- tests/testthat/test-DBItest.R
+++ tests/testthat/test-DBItest.R
@@ -4,12 +4,21 @@ DBItest::test_all(c(
   "show",                  # rstats-db/RPostgres#49
   "get_info",              # to be discussed
   "data_logical",          # not an error: no logical data type
+  "data_logical_null",     # not an error: no logical data type
+  "data_logical_int",      # not an error: no logical data type
+  "data_logical_int_null", # not an error: no logical data type
   "data_64_bit",           # rstats-db/RPostgres#51
+  "data_64_bit_null",      # rstats-db/RPostgres#51
   "data_character",        # #93
+  "data_character_null",   # #93
   "data_time",             # #95
+  "data_time_null",        # #95
   "data_time_parens",      # #95
+  "data_time_parens_null", # #95
   "data_timestamp_utc",    # syntax not supported
+  "data_timestamp_utc_null", # syntax not supported
   "data_timestamp_parens", # syntax not supported
+  "data_timestamp_parens_null", # syntax not supported
   "roundtrip_quotes",      # #101
   "roundtrip_logical",     # not an error: no logical data type
   "roundtrip_64_bit",      # rstats-db/DBI#48
