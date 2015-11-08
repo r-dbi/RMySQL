e637e11040f5f16072c670a77e906978dac16c2a
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-11-08 20:55:50 +0100

    DBItest: command_query

--- tests/testthat/test-queries.R
+++ tests/testthat/test-queries.R
@@ -28,18 +28,6 @@ test_that("correctly computes affected rows", {
   dbDisconnect(conn)
 })
 
-test_that("modification query is always complete", {
-  conn <- mysqlDefault()
-  dbWriteTable(conn, 'iris', datasets::iris, temporary = TRUE)
-  rs <- dbSendQuery(conn, "DELETE FROM iris WHERE Species = 'versicolor'")
-
-  expect_true(dbHasCompleted(rs))
-
-  dbClearResult(rs)
-  dbDisconnect(conn)
-})
-
-
 test_that("setting parameter query is always complete", {
   conn <- mysqlDefault()
   rs <- dbSendQuery(conn, 'SET time_zone = "+00:00"')
