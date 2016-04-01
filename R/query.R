1960a5fb55057700eaae0e518afdb8475db61d93
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2016-04-01 14:38:49 +0200

    reflect rename of DBI function

--- R/query.R
+++ R/query.R
@@ -14,7 +14,7 @@ NULL
 #'
 #' @param conn an \code{\linkS4class{MySQLConnection}} object.
 #' @param res A  \code{\linkS4class{MySQLResult}} object.
-#' @inheritParams DBI::rownamesToColumn
+#' @inheritParams DBI::sqlRownamesToColumn
 #' @param n Number of rows to retrieve. Use -1 to retrieve all rows.
 #' @param params A list of query parameters to be substituted into
 #'   a parameterised query.
