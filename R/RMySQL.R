9742acebbf2b559c74d66795389f80761803a58f
Kirill Müller <krlmlr+r@mailbox.org>
2016-12-29 09:34:28 +0100

    use _PACKAGE, CC @Ironholds

--- R/RMySQL.R
+++ R/RMySQL.R
@@ -1,10 +1,3 @@
-#' @title RMySQL: Database Interface and MySQL Driver for R
-#' @name RMySQL
-#' @description Implements a DBI interface to MySQL and MariaDB databases.
-#'
-#' @seealso the \href{https://cran.r-project.org/web/packages/RMySQL/RMySQL.pdf}{package documentation}.
 #' @useDynLib RMySQL
 #' @importFrom Rcpp sourceCpp
-#' @docType package
-#' @aliases RMySQL RMySQL-package
-NULL
+"_PACKAGE"
