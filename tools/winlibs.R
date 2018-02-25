c658644a0c6c2314e899b3ba8e4f4f93cbd6f8dc
Jeroen <jeroenooms@gmail.com>
2018-02-25 17:51:56 +0100

    Tweak configure

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -2,7 +2,7 @@
 # Info and source code: https://downloads.mariadb.org/client-native/
 if(!file.exists("../windows/libmariadbclient-2.3.4/include/mariadb/mysql.h")){
   if(getRversion() < "3.3.0") setInternet2()
-  curl::curl_download("https://github.com/rwinlib/libmariadbclient/archive/v2.3.4.zip", "lib.zip")
+  download.file("https://github.com/rwinlib/libmariadbclient/archive/v2.3.4.zip", "lib.zip", quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
   unzip("lib.zip", exdir = "../windows")
   unlink("lib.zip")
