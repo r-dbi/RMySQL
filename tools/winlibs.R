9a6dfeaf102b0090d817d10caae708d080956c21
Jeroen Ooms <jeroenooms@gmail.com>
2018-03-26 15:53:29 +0200

    Update libmariadb on Windows

--- tools/winlibs.R
+++ tools/winlibs.R
@@ -1,8 +1,8 @@
 # Link against mariadb native client static libraries
 # Info and source code: https://downloads.mariadb.org/client-native/
-if(!file.exists("../windows/libmariadbclient-2.3.4/include/mariadb/mysql.h")){
+if(!file.exists("../windows/libmariadbclient-2.3.5/include/mariadb/mysql.h")){
   if(getRversion() < "3.3.0") setInternet2()
-  curl::curl_download("https://github.com/rwinlib/libmariadbclient/archive/v2.3.4.zip", "lib.zip")
+  curl::curl_download("https://github.com/rwinlib/libmariadbclient/archive/v2.3.5.zip", "lib.zip")
   dir.create("../windows", showWarnings = FALSE)
   unzip("lib.zip", exdir = "../windows")
   unlink("lib.zip")
