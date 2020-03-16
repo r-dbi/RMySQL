5a63df3c0d942895483bfcb6ebcf46767db496ff
Andrew Kane <andrew@chartkick.com>
2020-03-15 22:43:52 -0700

    Moved on.exit

--- tests/testthat/test-dbQuoteString.R
+++ tests/testthat/test-dbQuoteString.R
@@ -4,7 +4,8 @@ test_that("quoting works", {
   if (!mysqlHasDefault()) skip("Test database not available")
 
   con <- dbConnect(MySQL(), dbname = "test")
+  on.exit(dbDisconnect(con))
+
   expect_equal(dbQuoteString(con, "\\'"), SQL("'\\\\\\''"))
   expect_equal(dbQuoteString(con, "'"), SQL("'\\''"))
-  on.exit(dbDisconnect(con))
 })
